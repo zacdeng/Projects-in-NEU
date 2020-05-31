4c0a0a1942ee32378b09caffea68ebf3f825e514
zacdeng <zacdeng0720@gmail.com>
2020-05-31 18:33:42 +0800

    CMakeFiles

---
+++ ATM_simulation/cmake-build-debug/CMakeFiles/Bank_simulate.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/BankAcc.cpp" "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/cmake-build-debug/CMakeFiles/Bank_simulate.dir/BankAcc.cpp.obj"
+  "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/UI_Design.cpp" "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/cmake-build-debug/CMakeFiles/Bank_simulate.dir/UI_Design.cpp.obj"
+  "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/main.cpp" "D:/study_software/CLion 2019.3.4/Clion_Projects/Bank_simulate/cmake-build-debug/CMakeFiles/Bank_simulate.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ATM_simulation/cmake-build-debug/CMakeFiles/Bank_simulate.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Bank_simulate.exe"
+  "Bank_simulate.exe.manifest"
+  "Bank_simulate.pdb"
+  "CMakeFiles/Bank_simulate.dir/BankAcc.cpp.obj"
+  "CMakeFiles/Bank_simulate.dir/UI_Design.cpp.obj"
+  "CMakeFiles/Bank_simulate.dir/main.cpp.obj"
+  "libBank_simulate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bank_simulate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
